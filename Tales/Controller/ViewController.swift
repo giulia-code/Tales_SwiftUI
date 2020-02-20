4c2227ee2892aaa53a7e294c567c4a18d0266991
giulia-code <giuliacatalano@me.com>
2020-02-20 15:19:18 +0100

    Initial Commit

---
+++ Tales/Controller/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Tales
+//
+//  Created by Macbook on 15/02/2020.
+//  Copyright © 2020 Giulia Catalano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    
+    @IBOutlet weak var choice1Button: UIButton!
+    
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+        
+    }
+    
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+    
+    
+}
+
---
+++ Tales/Model/Story.swift
@@ -0,0 +1,17 @@
+//
+//  Story.swift
+//  Tales
+//
+//  Created by Macbook on 15/02/2020.
+//  Copyright © 2020 Giulia Catalano. All rights reserved.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
